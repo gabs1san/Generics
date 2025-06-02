a5b8064e548fec348c28872ad511660566f9a497
gabs1san <sanbiel398@gmail.com>
2025-06-02 17:40:09 -0300

    Um

--- Program.cs
+++ Program.cs
@@ -7,7 +7,19 @@ namespace Generics
     {
         public static void Main(string[] args)
         {
+            PrintSerivce printService = new PrintSerivce();
 
+            Console.Write("How many values?: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());  
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.Fisrt()); 
         }
     }
 }
